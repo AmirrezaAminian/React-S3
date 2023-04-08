159a994d4a304a2502f5abd4f7104d83372f58fc
Amirreza Aminian <aminian.amirreza@gmail.com>
2023-04-08 17:14:16 +0430

    add dates and componet bt props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,4 +1,25 @@
-function ExpenseItem(){
-    return <h2>Expense item!</h2>
+import "./ExpenseItem.css";
+
+function ExpenseItem(props) {
+
+    const month = props.date.toLocaleString("en-US", { month: "long" });
+    const year = props.date.getFullYear();
+    const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+
+
+  return (
+    <div className="expense-item">
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </div>
+  );
 }
+
 export default ExpenseItem;
